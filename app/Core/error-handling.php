e03e5736eaee1ccefc00b06b00a401a4131596df
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2024-02-08 11:57:42 +0800

    update Core/error-handling:apply AppConfig::

--- app/Core/error-handling.php
+++ app/Core/error-handling.php
@@ -1,18 +1,34 @@
 <?php
 
+use MuhammadSabri1306\MyBotLogger\Entities\WarningLogger;
 use App\Core\Exception\PHPWarningException;
 use App\Core\Exception\PHPNoticeException;
+use App\Config\AppConfig;
 
 set_error_handler(function ($errNo, $errMsg, $errFile = null, $errLine = null, array $errContext = []) {
 
-    // if($errNo === E_WARNING) {
-    //     $err = new PHPWarningException("WARNING:$errMsg", 0);
-    //     \MuhammadSabri1306\MyBotLogger\Entities\WarningLogger::catch($err);
-    // }
+    if(AppConfig::$MODE == 'production') {
 
-    // if($errNo === E_NOTICE) {
-    //     $err = new PHPNoticeException("NOTICE:$errMsg", 0);
-    //     \MuhammadSabri1306\MyBotLogger\Entities\WarningLogger::catch($err);
-    // }
+        // if($errNo === E_WARNING) {
+        //     $err = new PHPWarningException("WARNING:$errMsg", 0);
+        //     \MuhammadSabri1306\MyBotLogger\Entities\WarningLogger::catch($err);
+        // }
+    
+        // if($errNo === E_NOTICE) {
+        //     $err = new PHPNoticeException("NOTICE:$errMsg", 0);
+        //     \MuhammadSabri1306\MyBotLogger\Entities\WarningLogger::catch($err);
+        // }
+
+    } elseif(AppConfig::$MODE == 'development') {
+
+        if($errNo === E_WARNING) {
+            throw new PHPWarningException("WARNING:$errMsg", 0);
+        }
+    
+        if($errNo === E_NOTICE) {
+            throw new PHPNoticeException("NOTICE:$errMsg", 0);
+        }
+
+    }
 
 });
