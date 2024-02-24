717a84638eff90345b8e8801b323fcecda4e9d87
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2024-02-24 19:06:41 +0800

    update PortController.getPortChart

--- app/Controller/Bot/PortController/get-port-chart.php
+++ app/Controller/Bot/PortController/get-port-chart.php
@@ -1,9 +1,13 @@
 <?php
 
 use Goat1000\SVGGraph\SVGGraph;
-use App\ApiRequest\NewosaseApi;
-use App\Helper\Helper;
+use MuhammadSabri1306\MyBotLogger\Entities\HttpClientLogger;
+use MuhammadSabri1306\MyBotLogger\Entities\ErrorLogger;
+use App\Libraries\HttpClient\Exceptions\ClientException;
+use App\Libraries\HttpClient\Exceptions\DataNotFoundException;
+use App\ApiRequest\NewosaseApiV2;
 use App\Config\AppConfig;
+use App\Helper\Helper;
 
 if(AppConfig::$MODE == 'production') {
     AppConfig::addErrorExclusions('notice', function($err) {
@@ -23,21 +27,45 @@ if(AppConfig::$MODE == 'production') {
 }
 
 $portChart = null;
+
 $currHourDateTime = new \DateTime();
 $currHourDateTime->setTime($currHourDateTime->format('H'), 0, 0);
-$fileName = 'checkport_chart_port_'.$portId.'_' . $currHourDateTime->getTimestamp();
+$fileNamePrefix = "checkport_chart_port_$portId";
+$fileName = $fileNamePrefix . '_' . $currHourDateTime->getTimestamp();
 
-$svgFileName = $fileName . '.svg';
-$svgFilePath = __DIR__.'/../../../../public/charts/'.$svgFileName;
+$svgFileName = "$fileName.svg";
+$svgFilePath = Helper::publicPath("charts/$svgFileName");
 
-$pngFileName = $fileName . '.png';
-$pngFilePath = __DIR__.'/../../../../public/charts/'.$pngFileName;
+$pngFileName = "$fileName.png";
+$pngFilePath = Helper::publicPath("charts/$pngFileName");
 
+// Check if chart on the current hour is exists
 if(file_exists($pngFilePath)) {
     $portChart = Helper::env('PUBLIC_URL', '') . "/public/charts/$pngFileName";
     return $portChart;
 }
 
+// Delete previous chart files
+try {
+    $dirName = Helper::publicPath('charts');
+    $dir = opendir($dirName);
+    while(( $searchedFileName = readdir($dir) ) !== false) {
+        if($searchedFileName == '.' || $searchedFileName == '..') {
+            continue;
+        }
+        if(strpos($searchedFileName, $fileNamePrefix) === 0) {
+            $matchedFilePath = $dirName . DIRECTORY_SEPARATOR . $searchedFileName;
+            if(file_exists($matchedFilePath)) {
+                unlink($matchedFilePath);
+            }
+        }
+    }
+    closedir($dir);
+} catch(\Throwable $err) {
+    static::logError( new ErrorLogger($err) );
+}
+
+// Generate new chart
 $currDateTime = new \DateTime('now', new \DateTimeZone('Asia/Jakarta'));
 $currDateTimeStr = $currDateTime->format('Y-m-d H:i:s');
 $currDateTime->setTime(0, 0, 0);
@@ -45,46 +73,64 @@ $currTimestamp = $currDateTime->getTimestamp();
 $endTime = $currTimestamp * 1000;
 $startTime = ($currTimestamp - (48 * 3600)) * 1000;
 
-$newosaseApi = new NewosaseApi();
-$newosaseApi->setupAuth();
-$newosaseApi->request['query'] = [
+$newosasePoolingUrlPath = "/dashboard-service/operation/chart/pooling/$portId";
+$newosasePoolingUrlParams = [
     'start' => $startTime,
     'end' => $endTime,
     'timeframe' => 'hour',
     'is_formula' => 0
 ];
 
-$poolingData = $newosaseApi->sendRequest('GET', "/dashboard-service/operation/chart/pooling/$portId");
-if(!$poolingData) {
-    $fetchErr = $newosaseApi->getErrorMessages()->response;
-    $errMsg = isset($fetchErr->message) ? $fetchErr->message : '';
-    $errCode = isset($fetchErr->code) ? $fetchErr->code : '';
-    throw new \Error("Newosase API error with code:$errCode, message: $errMsg");
+$newosaseApi = new NewosaseApiV2();
+$newosaseApi->setupAuth();
+$newosaseApi->request['query'] = $newosasePoolingUrlParams;
+
+$poolingData = [];
+try {
+
+    $osaseData = $newosaseApi->sendRequest('GET', $newosasePoolingUrlPath);
+    $poolingData = $osaseData->find('result', NewosaseApiV2::EXPECT_ARRAY_NOT_EMPTY);
+
+} catch(ClientException $err) {
+    static::logError( new HttpClientLogger($err) );
+    return null;
+} catch(DataNotFoundException $err) {
+    static::logError( new ErrorLogger($err) );
+    return null;
 }
 
+$newosaseTresholdUrlPath = "/dashboard-service/operation/port/treshold/$portId";
 $newosaseApi->request['query'] = [];
-$tresholdData = $newosaseApi->sendRequest('GET', "/dashboard-service/operation/port/treshold/$portId");
-if(!$tresholdData) {
-    $fetchErr = $newosaseApi->getErrorMessages()->response;
-    $errMsg = isset($fetchErr->message) ? $fetchErr->message : '';
-    $errCode = isset($fetchErr->code) ? $fetchErr->code : '';
-    throw new \Error("Newosase API error with code:$errCode, message: $errMsg");
-}
-
 $tresholdTop = null;
 $tresholdBottom = null;
+try {
 
-if($tresholdData->result->rules && count($tresholdData->result->rules) > 0) {
+    $osaseData = $newosaseApi->sendRequest('GET', $newosaseTresholdUrlPath);
+    $rules = $osaseData->find('result.rules', NewosaseApiV2::EXPECT_ARRAY_NOT_EMPTY);
 
-    $treshold = $tresholdData->result->rules[0];
-    $pattern = '/val\s*<\s*(\d+)\s*or\s*val\s*>\s*(\d+)/';
+    $tTresholds = [];
+    $bTresholds = [];
+    foreach($rules as $rule) {
 
-    if(preg_match($pattern, $treshold->rule, $tresholdMatches)) {
-        $tresholdBottom = (int)$tresholdMatches[1];
-        $tresholdTop = (int)$tresholdMatches[2];
+        if(preg_match('/val\s*<\s*(\d+)/', $rule, $matches)) {
+            $tresholdVal = ( $matches[1] === (int) $matches[1] ) ? (int) $matches[1] : (double) $matches[1];
+            array_push($tTresholds, $tresholdVal);
+        }
+
+        if(preg_match('/val\s*>\s*(\d+)/', $rule, $matches)) {
+            $tresholdVal = ( $matches[1] === (int) $matches[1] ) ? (int) $matches[1] : (double) $matches[1];
+            array_push($bTresholds, $tresholdVal);
+        }
+
     }
     
-}
+    if(!empty($tTresholds)) $tresholdTop = max($tTresholds);
+    if(!empty($bTresholds)) $tresholdBottom = min($bTresholds);
+
+} catch(ClientException $err) {
+    static::logError( new HttpClientLogger($err) );
+    return null;
+} catch(DataNotFoundException $err) {}
 
 $dataMax = [ 'title' => 'Maximum', 'color' => '#ff4560', 'dash' => null, 'data' => [] ];
 $dataAvg = [ 'title' => 'Average', 'color' => '#11d190', 'dash' => null, 'data' => [] ];
@@ -93,7 +139,7 @@ $dataLimitT = [ 'title' => 'Batas Atas', 'color' => '#008ffb', 'dash' => '5,3', 
 $dataLimitB = [ 'title' => 'Batas Bawah', 'color' => '#775dd0', 'dash' => '5,3', 'data' => [] ];
 
 $isDataEmpty = true;
-foreach($poolingData->result as $item) {
+foreach($poolingData as $item) {
 
     $itemDate = date('Y-m-d\TH:i', $item->timestamps / 1000);
 
@@ -131,10 +177,13 @@ if($tresholdBottom) {
     $dataLimitB['data'][$chartEndDate] = $tresholdBottom;
 }
 
-$isApplyLimit = count($dataLimitT['data']) > 0 && count($dataLimitB['data']) > 0;
 $chartData = [];
 
-if($isApplyLimit) array_push($chartData, $dataLimitT, $dataLimitB);
+$isApplyLimit = count($dataLimitT['data']) > 0 && count($dataLimitB['data']) > 0;
+if($isApplyLimit) {
+    array_push($chartData, $dataLimitT, $dataLimitB);
+}
+
 if(count($dataMax['data']) > 0) array_push($chartData, $dataMax);
 if(count($dataAvg['data']) > 0) array_push($chartData, $dataAvg);
 if(count($dataMin['data']) > 0) array_push($chartData, $dataMin);
@@ -226,56 +275,36 @@ $graph->colours(array_map(function($item) {
 $graph->values(array_map(fn($item) => $item['data'], $chartData));
 $svgImg = $graph->fetch('MultiLineGraph');
 
+$pngGeneratorCmd = "node /var/www/html/newopnimus/app/CLI/svg-to-png charts/$svgFileName";
 try {
 
-
-    if(file_put_contents($svgFilePath, $svgImg)) {
-
-        $cmd = "node /var/www/html/newopnimus/app/CLI/svg-to-png charts/$svgFileName";
-        $output = null;
-
-        if(exec($cmd, $output)) {
-            
-            $portChart = Helper::env('PUBLIC_URL', '') . "/public/charts/$pngFileName";
-            return $portChart;
-    
-        }
-        
-        static::sendDebugMessage([
-            'status' => 'Cannot write png file',
-            'png_path' => realpath($pngFilePath),
-            'api' => [
-                [
-                    'path' => "/dashboard-service/operation/chart/pooling/$portId",
-                    'params' => [
-                        'start' => $startTime,
-                        'end' => $endTime,
-                        'timeframe' => 'hour',
-                        'is_formula' => 0
-                    ]
-                ]
-            ]
-        ]);
-
+    if(file_put_contents($svgFilePath, $svgImg) === false) {
+        throw new \Exception('Fail to write svg file');
     }
 
-    static::sendDebugMessage([
-        'status' => 'Cannot write svg file',
-        'svg_path' => realpath($svgFilePath),
-        'api' => [
-            [
-                'path' => "/dashboard-service/operation/chart/pooling/$portId",
-                'params' => [
-                    'start' => $startTime,
-                    'end' => $endTime,
-                    'timeframe' => 'hour',
-                    'is_formula' => 0
-                ]
-            ]
+    $output = null;
+    if(exec($pngGeneratorCmd, $output) === false) {
+        throw new \Exception('Fail to write png file');
+    }
+
+    $portChart = Helper::publicPath("charts/$pngFileName");
+    return $portChart;
+
+} catch(\Throwable $err) {
+
+    $logger = new ErrorLogger($err);
+    $logger->setParams([
+        'png_path' => $pngFilePath,
+        'svg_path' => $svgFilePath,
+        'png_generator' => $pngGeneratorCmd,
+        'apis' => [
+            [ 'path' => $newosasePoolingUrlPath, 'params' => $newosasePoolingUrlParams ],
+            [ 'path' => $newosaseTresholdUrlPath ],
         ]
     ]);
 
-} catch(\Throwable $err) {
-    static::logError( new \MuhammadSabri1306\MyBotLogger\Entities\ErrorLogger($err) );
+    static::logError($logger);
+
 }
+
 return $portChart;
